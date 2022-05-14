0d8b2425965e378bd36a78f08d63c757c511fb2b
kieranmichaelflynn <kieranmichaelflynn@gmail.com>
2022-05-14 10:39:54 +0800

    initial commit

---
+++ models/ProductTag.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+const Tag = require('./Tag');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      reference: {
+        key: 'id',
+        model: ProductTag,
+      }
+    },
+    tag_id: {
+      type: DataTypes.STRING,
+      model: Tag,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
---
+++ models/index.js
@@ -0,0 +1,32 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category);
+
+// Categories have many Products
+Category.hasMany(Product);
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  as: 'product_tags',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: Tag,
+  foreignKey: 'tag_id',
+  as: 'product_tags',
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
